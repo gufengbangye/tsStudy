48fa3a75d59c6df6360155b9578e26b7f4d09c6c
gufengbangye <1135523598@qq.com>
2022-06-07 21:31:52 +0800

    开始学习

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+    entry:{
+        index:'./src/index.ts'
+    },
+    output:{
+        filename:'build.[name].js',
+        path: path.resolve(__dirname,'dist')
+    },
+    module:{
+        rules:[{
+            test:/\.ts$/,
+            use:['ts-loader'],
+            exclude: /.node_modules/
+        }]
+    },
+    mode:'development',
+    resolve:{
+        extensions:['.ts','.tsx','.js']
+    }
+}
